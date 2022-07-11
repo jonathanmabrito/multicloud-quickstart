b8436f78543e51c362c2b0dc55f8f4d021a576cd
Jonathan Mabrito <jonathan.mabrito@genesys.com>
2022-07-11 16:02:32 -0400

    Installing Voice

--- services/provision-voice.sh
+++ services/provision-voice.sh
@@ -16,7 +16,7 @@ export SERVICE=voice
 export DOMAIN=cluster03.gcp.demo.genesys.com
 export IMAGE_REGISTRY=gcr.io/gts-multicloud-pe-dev/gts-multicloud-pe
 export ARTIFACT_REPO=oci://us-west2-docker.pkg.dev/gts-multicloud-pe-dev/gts-multicloud-pe
-export FULLCOMMAND=uninstall
+export FULLCOMMAND=install
 
 echo "***********************"
 echo "Create or use namespace"

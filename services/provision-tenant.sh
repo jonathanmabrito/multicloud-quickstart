0f599e8d091c3a9ce815f28a6bf29f3be994f8c1
Dmitry Mizin <38381375+dmizin@users.noreply.github.com>
2022-07-14 13:38:51 -0700

    SERVICE=tenant

--- services/provision-tenant.sh
+++ services/provision-tenant.sh
@@ -12,7 +12,7 @@ echo "***********************"
 echo "Setting Variables"
 echo "***********************"
 export NS=voice
-export SERVICE=voice
+export SERVICE=tenant
 export DOMAIN=cluster02.gcp.demo.genesys.com
 export IMAGE_REGISTRY=gcr.io/gts-multicloud-pe-dev/gts-multicloud-pe
 export ARTIFACT_REPO=oci://us-west2-docker.pkg.dev/gts-multicloud-pe-dev/gts-multicloud-pe

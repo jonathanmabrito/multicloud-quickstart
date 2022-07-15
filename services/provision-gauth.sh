838b91add2e0b20c5e4a5e26490cd3180c6a01df
Jonathan Mabrito <jonathan.mabrito@genesys.com>
2022-07-15 13:49:38 -0400

    Uninstall gauth

--- services/provision-gauth.sh
+++ services/provision-gauth.sh
@@ -51,7 +51,7 @@ export SERVICE=gauth
 export DOMAIN=cluster03.gcp.demo.genesys.com
 export IMAGE_REGISTRY=gcr.io/gts-multicloud-pe-dev/gts-multicloud-pe
 export ARTIFACT_REPO=oci://us-west2-docker.pkg.dev/gts-multicloud-pe-dev/gts-multicloud-pe
-export FULLCOMMAND=install
+export FULLCOMMAND=uninstall
 
 cd "./services/$SERVICE"
 COMMAND=$(echo $FULLCOMMAND | cut -d' ' -f1)

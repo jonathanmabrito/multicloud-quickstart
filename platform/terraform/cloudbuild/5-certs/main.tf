f516ffcfe5dd557c4cf72248d07b2f041a20b0fd
Jonathan Mabrito <95763642+jonathanmabrito@users.noreply.github.com>
2022-07-19 20:22:40 -0400

    Changing Clusters (working on Build Variables)

--- platform/terraform/cloudbuild/5-certs/main.tf
+++ platform/terraform/cloudbuild/5-certs/main.tf
@@ -1,7 +1,7 @@
 module "gke_auth" {
   source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  project_id    = "gts-multicloud-pe-dev"
-  cluster_name  = "cluster02"
+  project_id    = "gts-multicloud-pe-dev2"
+  cluster_name  = "cluster03"
   location      = "us-west2"
 }
 
@@ -12,9 +12,9 @@ resource "local_file" "kubeconfig" {
 
 module "ingress_certs" {
   source            = "../../../tfm/5-ingress-certs/"
-  project_id        = "gts-multicloud-pe-dev"
-  environment       = "gts-multicloud-pe-dev" #same value as in 1-network
-  domain_name_nginx = "nlb02-uswest2.cluster02.gcp.demo.genesys.com" #domain.example.com should be same as FQDN in module 1-network
+  project_id        = "gts-multicloud-pe-dev2"
+  environment       = "gts-multicloud-pe-dev2" #same value as in 1-network
+  domain_name_nginx = "nlb02-uswest2.cluster03.gcp.demo.genesys.com" #domain.example.com should be same as FQDN in module 1-network
   email             = "jonathan.mabrito@genesys.com"
 }
 
@@ -22,17 +22,17 @@ module "ingress_certs" {
 
 data "google_client_config" "provider" {}
 
-data "google_container_cluster" "cluster02" {
-  name = "cluster02"
+data "google_container_cluster" "cluster03" {
+  name = "cluster03"
   location = "us-west2"
-  project = "gts-multicloud-pe-dev"
+  project = "gts-multicloud-pe-dev2"
 }
 
 provider "kubernetes" {
-  host = "https://${data.google_container_cluster.cluster02.endpoint}"
+  host = "https://${data.google_container_cluster.cluster03.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.cluster02.master_auth[0].cluster_ca_certificate,
+    data.google_container_cluster.cluster03.master_auth[0].cluster_ca_certificate,
   ) 
 }
 
@@ -44,10 +44,10 @@ default = "v2.9.1"
 
 provider "helm" {
   kubernetes {
-    host = "https://${data.google_container_cluster.cluster02.endpoint}"
+    host = "https://${data.google_container_cluster.cluster03.endpoint}"
     token = data.google_client_config.provider.access_token
     cluster_ca_certificate = base64decode(
-    data.google_container_cluster.cluster02.master_auth[0].cluster_ca_certificate,
+    data.google_container_cluster.cluster03.master_auth[0].cluster_ca_certificate,
     )
     config_path = "${path.module}/kubeconfig"
   }
@@ -55,7 +55,7 @@ provider "helm" {
 
 
 provider "google" {
-  project = "gts-multicloud-pe-dev"
+  project = "gts-multicloud-pe-dev2"
 }
 
 terraform {
@@ -71,7 +71,7 @@ terraform {
 
 terraform {
   backend "gcs" {
-    bucket = "gts-multicloud-pe-dev-tf-statefiles" #Replace with the name of the bucket created above
-    prefix = "ingress-certs-cluster02-uswest2-state" #creates a new folder
+    bucket = "gts-multicloud-pe-dev2-tf-statefiles" #Replace with the name of the bucket created above
+    prefix = "ingress-certs-cluster03-uswest2-state" #creates a new folder
   }
 }

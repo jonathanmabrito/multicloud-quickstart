17197b02a9f35cb2417f3ae4d105a3c68eec9e56
Dmitry Mizin <dmitry.mizin@genesys.com>
2022-07-24 18:29:59 -0700

    image pull, tag, push test

--- platform/tfm/9-repo/main.tf
+++ platform/tfm/9-repo/main.tf
@@ -1,7 +1,15 @@
-resource "google_artifact_registry_repository" "my-repo" {
-  location      = var.region
-  project       = var.project
-  repository_id = var.repoid
-  description   = "Genesys container and helm chart repository"
-  format        = "DOCKER"
+#resource "google_artifact_registry_repository" "my-repo" {
+#  location      = var.region
+#  project       = var.project
+#  repository_id = var.repoid
+#  description   = "Genesys container and helm chart repository"
+#  format        = "DOCKER"
+#}
+
+resource "null_resource" "image" {
+  # ...
+
+  provisioner "local-exec" {
+    command = "podman pull hello-world | podman tag hello-world us-west2-docker.pkg.dev/gts-multicloud-pe-dmitry/gts-multicloud-pe/hello-world | podman push us-west2-docker.pkg.dev/gts-multicloud-pe-dmitry/gts-multicloud-pe/hello-world"
+  }
 }

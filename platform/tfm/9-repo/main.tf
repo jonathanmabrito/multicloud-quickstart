5abfed0ca2b0af5bf5b8df7f8577cbaed811e621
Dmitry Mizin <dmitry.mizin@genesys.com>
2022-07-24 18:53:48 -0700

    image pull, tag, push command fix

--- platform/tfm/9-repo/main.tf
+++ platform/tfm/9-repo/main.tf
@@ -8,8 +8,12 @@
 
 resource "null_resource" "image" {
   # ...
-
+  interpreter = ["/bin/bash", "-c"]
   provisioner "local-exec" {
-    command = "podman pull docker.io/library/hello-world:latest | podman tag docker.io/library/hello-world:latest us-west2-docker.pkg.dev/gts-multicloud-pe-dmitry/gts-multicloud-pe/hello-world | podman push us-west2-docker.pkg.dev/gts-multicloud-pe-dmitry/gts-multicloud-pe/hello-world:latest"
+    command = <<EOT
+      "podman pull docker.io/library/hello-world:latest"
+      "podman tag docker.io/library/hello-world:latest us-west2-docker.pkg.dev/gts-multicloud-pe-dmitry/gts-multicloud-pe/hello-world"
+      "podman push us-west2-docker.pkg.dev/gts-multicloud-pe-dmitry/gts-multicloud-pe/hello-world:latest"
+    EOT
   }
 }

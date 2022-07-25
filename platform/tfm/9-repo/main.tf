f785c3c55614999eb6298bba5b4df45dd3343667
Dmitry Mizin <dmitry.mizin@genesys.com>
2022-07-25 11:26:52 -0700

    repo exists check before creating

--- platform/tfm/9-repo/main.tf
+++ platform/tfm/9-repo/main.tf
@@ -1,10 +1,19 @@
-#resource "google_artifact_registry_repository" "my-repo" {
-#  location      = var.region
-#  project       = var.project
-#  repository_id = var.repoid
-#  description   = "Genesys container and helm chart repository"
-#  format        = "DOCKER"
-#}
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = var.region
+  project       = var.project
+  repository_id = var.repoid
+  description   = "Genesys container and helm chart repository"
+  format        = "DOCKER"
+  lifecycle {
+    # Only run if doesn't exists
+    precondition {
+      condition     = "gcloud artifacts repositories describe ${var.repoid} --location=${var.region}" == 0
+      error_message = "Repo already exists"
+    }
+  }
+}
+
+
 
 resource "null_resource" "image" {
   # ...

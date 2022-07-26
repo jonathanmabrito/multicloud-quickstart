12509fd23f30afd46de009d40336efe1c92ce9c5
Dmitry Mizin <dmitry.mizin@genesys.com>
2022-07-25 21:10:08 -0700

    image, helm version updates

--- platform/tfm/9-repo/variables.tf
+++ platform/tfm/9-repo/variables.tf
@@ -34,17 +34,27 @@ variable "remoteregistry_pass" {
 }
 
 variable "images" {
-    type = set(string)
-    default = [
-        #"voice/agent_node:100.0.1000013",
-        #"voice/callthread_node:100.0.1000013",
-        #"voice/config_node:100.0.1000010"
-        ]
+    type = map
+    default = {
+        #gauth version not on edge
+        "voice/agent_node" = "100.0.1000013"
+        "voice/callthread_node" = "100.0.1000013"
+        "voice/config_node" = "100.0.1000010"
+        "voice/dialplan_node" = "100.0.1000015"
+        "voice/ors_node" = "100.0.1000056"
+        "voice/registrar_node" = "100.0.1000019"
+        "voice/rq_node" = "100.0.1000010"
+        "voice/sip_node" = "100.0.1000030"
+        "voice/sipfe_node" = "100.0.1000009"
+        "voice/sipproxy_node" = "100.0.1000019"
+        "voice/voicemail_node" = "100.0.1000006"
+    }
 }
 
 variable "charts" {
     type = map
     default = {
+        # gauth version not on edge
         "voice-agent" = "100.0.1000005"
         "voice-callthread" = "100.0.1000006"
         "voice-config" = "100.0.1000004"
@@ -55,6 +65,7 @@ variable "charts" {
         "voice-sip" = "100.0.1000017"
         "voice-sipfe" = "100.0.1000006"
         "voice-sipproxy" = "100.0.1000004"
-        "voice-voicemail" = "100.0.1000011"
+        # voicemail 100.0.1000012 not on edge
+        # "voice-voicemail" = "100.0.1000011"
     }
 }

035bbd7cf1fafca66fc170b21384a08b1713423b
Dmitry Mizin <dmitry.mizin@genesys.com>
2022-07-25 17:11:15 -0700

    repo fix

--- platform/tfm/9-repo/main.tf
+++ platform/tfm/9-repo/main.tf
@@ -7,23 +7,22 @@ resource "google_artifact_registry_repository" "my-repo" {
   format        = "DOCKER"
 }
 
-#resource "null_resource" "remoteregistry" {
+resource "null_resource" "remoteregistry" {
   # Login to remote registry
-  #provisioner "local-exec" {
-  #  command = "podman login -u ${var.remoteregistry_user} -p ${var.remoteregistry_pass} ${var.remoteregistry}"
-  #}
-#}
+  provisioner "local-exec" {
+    command = "podman login -u ${var.remoteregistry_user} -p ${var.remoteregistry_pass} ${var.remoteregistry}"
+  }
+}
 
 resource "null_resource" "image" {
   # Pull, tag, push containers
   for_each = var.images
   provisioner "local-exec" {
     command = <<-EOT
-      podman login -u ${var.remoteregistry_user} -p ${var.remoteregistry_pass} ${var.remoteregistry}
-      podman pull docker.io/${each.value}
+      podman pull ${each.value}
       podman tag ${each.value} ${var.region}-docker.pkg.dev/${var.project}/${var.repoid}/${each.value}
       podman push ${each.value} ${var.region}-docker.pkg.dev/${var.project}/${var.repoid}/${each.value}
     EOT
   }
-  #depends_on = [null_resource.remoteregistry]
+  depends_on = [null_resource.remoteregistry]
 }

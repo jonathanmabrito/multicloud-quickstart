1efa59a2e2f3904b7e342324f9b58a14687e6deb
Jonathan Mabrito <jonathan.mabrito@genesys.com>
2022-07-28 08:11:29 -0400

    Swapped some openshift commands back to kubectl

--- services/ucsx/01_chart_ucsx/01_release_ucsx/post-release-script.sh
+++ services/ucsx/01_chart_ucsx/01_release_ucsx/post-release-script.sh
@@ -8,14 +8,14 @@
 echo "##################### 2. Validate that all pods have status running"
 for i in {1..12}; do
   echo "waiting for pods to get running.." && sleep 5
-  CSTAT=$(oc get pods -l service=ucsx,tenant=shared  --no-headers | awk '{print $3}' | sort | uniq)
+  CSTAT=$(kubectl get pods -l service=ucsx,tenant=shared  --no-headers | awk '{print $3}' | sort | uniq)
   [[ $CSTAT == "Running" ]] && break
   [[ $i == 12 ]] && echo "ERROR: can't get pods in running status" && exit 1
 done
 echo "successfully got all pods in running status"
 
 echo "##################### 3. Quick Healthcheck"
-for pod in $(oc get pods -l service=ucsx,tenant=shared  --no-headers | grep Running | awk '{print $1}'); do
+for pod in $(kubectl get pods -l service=ucsx,tenant=shared  --no-headers | grep Running | awk '{print $1}'); do
   echo "healthcheck for $pod"
   for i in {1..12}; do
     echo "waiting.." && sleep 5

fffefcbf262a87ea78dec8560df96757985b4ad6
Jonathan Mabrito <jonathan.mabrito@genesys.com>
2022-07-29 12:13:54 -0400

    Pulse work

--- platform/tfm/1-cloudbuild/main.tf
+++ platform/tfm/1-cloudbuild/main.tf
@@ -660,3 +660,67 @@ resource "google_cloudbuild_trigger" "services-tlm" {
     _VIMAGEREGISTRY = "gcr.io/gts-multicloud-pe-dev/gts-multicloud-pe"
   }
 }
+
+resource "google_cloudbuild_trigger" "services-pulse" {
+  project = var.project_id
+  name = "21-Provision-PrivateEdition-Applications-Pulse"
+
+  source_to_build {
+    uri       = "https://github.com/jonathanmabrito/multicloud-quickstart"
+    ref       = "refs/heads/development"
+    repo_type = "GITHUB"
+  }
+
+  git_file_source {
+    path      = "cloudbuild-services-pulse.yaml"
+    uri       = "https://github.com/jonathanmabrito/multicloud-quickstart"
+    revision  = "refs/heads/development"
+    repo_type = "GITHUB"
+  }
+
+  approval_config {
+    approval_required = false
+  }
+
+  substitutions = {
+    _VARTIFACTREPO  = "oci://us-west2-docker.pkg.dev/gts-multicloud-pe-dev/gts-multicloud-pe"   
+    _VDOMAIN        = var.fqdn
+    _VGCPPROJECT    = var.project_id
+    _VGCPREGION     = var.gkeregionprimary
+    _VGKECLUSTER    = var.gkecluster
+    _VHELMCOMMAND   = "install"
+    _VIMAGEREGISTRY = "gcr.io/gts-multicloud-pe-dev/gts-multicloud-pe"
+  }
+}
+
+resource "google_cloudbuild_trigger" "services-gcxi" {
+  project = var.project_id
+  name = "22-Provision-PrivateEdition-Applications-GCXI"
+
+  source_to_build {
+    uri       = "https://github.com/jonathanmabrito/multicloud-quickstart"
+    ref       = "refs/heads/development"
+    repo_type = "GITHUB"
+  }
+
+  git_file_source {
+    path      = "cloudbuild-services-gcxi.yaml"
+    uri       = "https://github.com/jonathanmabrito/multicloud-quickstart"
+    revision  = "refs/heads/development"
+    repo_type = "GITHUB"
+  }
+
+  approval_config {
+    approval_required = false
+  }
+
+  substitutions = {
+    _VARTIFACTREPO  = "oci://us-west2-docker.pkg.dev/gts-multicloud-pe-dev/gts-multicloud-pe"   
+    _VDOMAIN        = var.fqdn
+    _VGCPPROJECT    = var.project_id
+    _VGCPREGION     = var.gkeregionprimary
+    _VGKECLUSTER    = var.gkecluster
+    _VHELMCOMMAND   = "install"
+    _VIMAGEREGISTRY = "gcr.io/gts-multicloud-pe-dev/gts-multicloud-pe"
+  }
+}

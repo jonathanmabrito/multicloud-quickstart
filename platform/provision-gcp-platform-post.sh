0e7f6b387e7347140d975af251384de58316b011
Jonathan Mabrito <jonathan.mabrito@genesys.com>
2022-08-02 20:50:40 -0400

    Update to IF condition

--- platform/provision-gcp-platform-post.sh
+++ platform/provision-gcp-platform-post.sh
@@ -132,9 +132,9 @@ echo "***********************"
 # Checking for status of the cluster and performing 10 minute waits and re checks. 
 
 gkeClusterStatus=$(gcloud container clusters list --format="value(STATUS.scope())")
-if [echo $gkeClusterStatus != "RUNNING"]
+if ["$gkeClusterStatus" != "RUNNING"]
 then
-    until [ echo $gkeClusterStatus == "RUNNING" ]
+    until ["$gkeClusterStatus" == "RUNNING" ]
     do
     echo "GKE Cluster is not fully ready yet. Waiting 10 minutes to check again"
     sleep 10m

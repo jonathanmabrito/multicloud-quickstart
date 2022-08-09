771b2f963b5927935a6c524b91d8d5363b642e3d
Jonathan Mabrito <jonathan.mabrito@genesys.com>
2022-08-09 12:49:48 -0400

    changes

--- platform/terraform/3-gcp-posttasks/4-pullsecret/main.tf
+++ platform/terraform/3-gcp-posttasks/4-pullsecret/main.tf
@@ -14,6 +14,14 @@ terraform {
       source  = "hashicorp/google"
       version = "4.29.0"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.6.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.12.1"
+    }
   }
 
   required_version = "= 1.2.6"
